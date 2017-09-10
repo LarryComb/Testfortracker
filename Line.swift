29ce83f8aa3c42589b3b25e760d91339dbf1998d
“larrycomb” <larrycomb@gmail.com>
2017-09-10 13:14:12 -0700

    add the tracker files

---
+++ Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by LARRY COMBS on 3/23/17.
+//  Copyright © 2017 LARRY COMBS. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
